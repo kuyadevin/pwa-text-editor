44db98fa85f29b7cffc84ebfd8abfecf46694342
KuyaDevin <devinnguyen@ucsb.edu>
2022-05-25 15:34:46 -0700

    Added get method to database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,30 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB("jate", 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => console.error("putDb not implemented");
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log("GET from the database");
+  const contactDb = await openDB("contact", 1);
+  const tx = contactDb.transaction("contact", "readonly");
+  const store = tx.objectStore("contact");
+  const request = store.getAll();
+  const result = await request;
+  console.log("result.value", result);
+  return result;
+};
 
 initdb();
