125e92f8318dbfa6ec00c511843a541fa544f800
KuyaDevin <devinnguyen@ucsb.edu>
2022-05-25 15:23:23 -0700

    Added css and babel loaders

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -52,7 +52,28 @@ module.exports = () => {
     ],
 
     module: {
-      rules: [],
+      rules: [
+        //CSS loader
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        //Babel loader
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
+            },
+          },
+        },
+      ],
     },
   };
 };
