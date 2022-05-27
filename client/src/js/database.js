484dc91bee6c83be5a6d090434a4e7e0cc2f0fd7
KuyaDevin <devinnguyen@ucsb.edu>
2022-05-26 18:31:35 -0700

    Fixed varaible names in database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -15,20 +15,20 @@ const initdb = async () =>
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   console.log("PUT to the database");
-  const contactDb = await openDB("jate", 1);
-  const tx = contactDb.transaction("jate", "readwrite");
+  const jateDb = await openDB("jate", 1);
+  const tx = jateDb.transaction("jate", "readwrite");
   const store = tx.objectStore("jate");
-  const request = store.put({ value: content });
+  const request = store.put({ content: content });
   const result = await request;
   console.log("result.value", result);
-  return result;
+  // return result;
 };
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.log("GET from the database");
-  const contactDb = await openDB("jate", 1);
-  const tx = contactDb.transaction("jate", "readonly");
+  const jateDb = await openDB("jate", 1);
+  const tx = jateDb.transaction("jate", "readonly");
   const store = tx.objectStore("jate");
   const request = store.getAll();
   const result = await request;
